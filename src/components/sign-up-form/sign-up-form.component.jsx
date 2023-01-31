aa6f4fb55f9d9470ecc375ced96970f347f5def1
npdev <sashko.dev@gmail.com>
2023-01-31 18:41:57 +0000

    FormInput component + styles

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,7 @@
 import {useState} from 'react';
 
+import FormInput from '../form-input/form-input.component';
+
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth
@@ -48,14 +50,38 @@ const SignUpForm = () => {
     <div>
       <h1>Sign up</h1>
       <form onSubmit={handleSubmit}>
-        <label>Display Name</label>
-        <input type='text' required onChange={handleChange} name='displayName' value={displayName}/>
-        <label>Email</label>
-        <input type='email' required onChange={handleChange} name='email' value={email}/>
-        <label>Password</label>
-        <input type='password' required onChange={handleChange} name='password' value={password}/>
-        <label>Confirm Password</label>
-        <input type='password' required onChange={handleChange} name='confirmPassword' value={confirmPassword}/>
+        <FormInput
+          label='Display Name'
+          type='text'
+          required
+          onChange={handleChange}
+          name='displayName'
+          value={displayName}
+        />
+        <FormInput
+          label='Email'
+          type='email'
+          required
+          onChange={handleChange}
+          name='email'
+          value={email}
+        />
+        <FormInput
+          label='Password'
+          type='password'
+          required
+          onChange={handleChange}
+          name='password'
+          value={password}
+        />
+        <FormInput
+          label='Confirm Password'
+          type='password'
+          required
+          onChange={handleChange}
+          name='confirmPassword'
+          value={confirmPassword}
+        />
         <button type='submit'>Sign Up</button>
       </form>
     </div>
