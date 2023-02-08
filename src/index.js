ce6d23efbb428a8a613eeea7621d1123f651393b
npdev <sashko.dev@gmail.com>
2023-02-08 18:28:43 +0000

    Installing Redux + implementing user reducer

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 
 import App from './App';
-import {UserProvider} from './context/user.context';
-import {CategoriesProvider} from './context/categories.context';
+import {store} from './store/store';
 import {CartProvider} from './context/cart.context';
+import {CategoriesProvider} from './context/categories.context';
 
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
+    <Provider store={store}>
+      <BrowserRouter>
         <CategoriesProvider>
           <CartProvider>
             <App/>
           </CartProvider>
         </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
