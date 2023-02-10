c89a332bd3cf0d7133d1688abb5d28c9e9ddd005
npdev <sashko.dev@gmail.com>
2023-02-10 16:22:57 +0000

    Using redux-thunk + Spinner

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,17 +2,22 @@ import {useEffect, useState, Fragment} from 'react';
 import {useParams} from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
 import {
   CategoryContainer,
   Title
 } from './category.styles';
 import {useSelector} from 'react-redux';
-import {selectCategoriesMap} from '../../store/categories/category.selector';
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading
+} from '../../store/categories/category.selector';
 
 const Category = () => {
   const {category} = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -22,11 +27,14 @@ const Category = () => {
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => <ProductCard key={product.id} product={product}/>)
-        }
-      </CategoryContainer>
+      {
+        isLoading ? <Spinner/> :
+          <CategoryContainer>
+            {products &&
+              products.map((product) => <ProductCard key={product.id} product={product}/>)
+            }
+          </CategoryContainer>
+      }
     </Fragment>
   );
 }
--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,7 +1,9 @@
 import {CATEGORIES_ACTION_TYPES} from './category.types';
 
 export const CATEGORIES_INITIAL_STATE = {
-  categories: []
+  categories: [],
+  isLoading: false,
+  error: null,
 }
 
 export const categoriesReducer = (
@@ -9,11 +11,23 @@ export const categoriesReducer = (
   action = {}) => {
   const {type, payload} = action;
 
-  switch(type) {
-    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
+  switch (type) {
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
       return {
         ...state,
-        categories: payload
+        isLoading: true
+      };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+      return {
+        ...state,
+        categories: payload,
+        isLoading: false,
+      };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+      return {
+        ...state,
+        error: payload,
+        isLoading: false,
       };
     default:
       return state;
