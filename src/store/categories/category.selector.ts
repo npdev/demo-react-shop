23622d8ba00db29b8a54a6f4cac83aa619ed50d4
npdev <sashko.dev@gmail.com>
2023-02-21 15:33:26 +0000

    Typing store/categories except saga

--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -1,19 +1,23 @@
 import {createSelector} from 'reselect';
 
-const selectCategoryReducer = (state) => state.categories;
+import {CategoriesState} from './category.reducer';
+import {CategoryMap} from './category.types';
+
+const selectCategoryReducer = (state): CategoriesState => state.categories;
 
 export const selectCategories = createSelector(
   [selectCategoryReducer],
   (categoriesSlice) => categoriesSlice.categories
-)
+);
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) => categories.reduce((acc, category) => {
-    const {title, items} = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {})
+  (categories): CategoryMap =>
+    categories.reduce((acc, category) => {
+      const {title, items} = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {} as CategoryMap)
 );
 
 export const selectCategoriesIsLoading = createSelector(
